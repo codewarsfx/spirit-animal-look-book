ba650f5e4581676232ea259997d2afd5f664cf52
codewarsfx <chiderainnocent001@gmail.com>
2021-01-09 00:36:03 +0100

    implemented storage with firebase

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -4,24 +4,34 @@ import { storage, database } from './firebase';
 import './ProfileCard.css';
 
 class ProfileCard extends Component {
+
   constructor(props) {
     super(props);
     this.handleSubmit=this.handleSubmit.bind(this);
+    this.userRef=null;
+    
   }
 
   handleSubmit(e){
-
     const file = e.target.files[0];
+    const fileUploadTask= storage.ref('userpictures').child(`${this.props.uid}`)
+                                                      .child(this.props.user.name)
+                                                      .put(file);
+    fileUploadTask.then((snapshot)=>{     
+      this.userRef= database.ref(`users/${this.props.uid}`)
+                            .child('photoURL').set(snapshot.downloadURL);
+    });
+    
 
   }
 
   render() {
-    const {photoURL,displayName}= this.props.user;
+    const {photoURL,name}= this.props.user;
 
     return (
       <article className="ProfileCard">
         <img src={photoURL} className='ProfileCard--photo'/>
-        <h3>{displayName}</h3>
+        <h3>{name}</h3>
         <FileInput
         placeholder= 'change profile image'
         onChange={this.handleSubmit}
